b364497e18efafe0fed9ff8df5f1752893faa370
kjchacon <60801213+kjchacon@users.noreply.github.com>
2021-02-28 18:17:18 -0700

    Update GitDemo.java

--- GitDemo.java
+++ GitDemo.java
@@ -1,5 +1,5 @@
 /*****
- * Edit this line to have your name.
+Kyler Chacon
  ******/
 public class GitDemo {
     
@@ -14,7 +14,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        ans = num + previousNumber;
+        return ans;
     }
     
     /*****
